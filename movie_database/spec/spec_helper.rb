8855b5ea605d66d6de614fc8a0cdf70ee3a20388
Mario Goodwin <mr.goodwin@outlook.com>
2018-01-11 04:03:31 -0500

    Added tests to both backend APIs

--- movie_database/spec/spec_helper.rb
+++ movie_database/spec/spec_helper.rb
@@ -14,9 +14,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  # config.after(:suite) do
-  #   FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
-  # end
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
+  end
 
 
   # rspec-expectations config goes here. You can use an alternate
